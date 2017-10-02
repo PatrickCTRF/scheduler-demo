30cb46e75bc026bc72c630f7fd81738ea1de6ef6
Patrick Ferreira <patrickctrf@hotmail.com.br>
2017-10-02 14:44:08 -0300

    Arquivo funcionando com 100% de acerto nos testes.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
-  int n;
+  int a[5] = {0};
+  int n = 0, j = 0, i = 0;
+  int flag_ja_imprimiu = 0;//Esta flag indica se já imprimimos na aual iteração. Para facilitar a impressão ou nao do caractere "-".
+  							//Vale 1 quando já imprimimos e 0 quando não.
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+    
+    flag_ja_imprimiu = 0;
+    
+    for (j=0; j<5; j++){
+    	
+    	if(i%a[j] == 0){
+    		
+    		printf("%d", j);
+    		flag_ja_imprimiu = 1;
+    		
+    	}
+    	
+    }
+    
+    if(flag_ja_imprimiu != 1){
+    	
+    	printf("-");
+    	
+	}
+	
+	printf("\n");
+    
   }
 
   return 0;
